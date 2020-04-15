be9462b0a50f753fc5914be6c657f1925b656017
Jamie Symonds-Tayler <jamie.s-t@outlook.com>
2020-04-15 09:08:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,17 @@
 # require modules here
+require "yaml"
 
-def load_library
+def load_library(filepath)
   # code goes here
+  rawYAML = YAML.load_file(filepath)
+  libHash = {}
+  rawYAML.each do |key,value|
+    libHash[key.to_sym]={
+      english: value[0],
+      japanese: value[1]
+    }
+  end
+  libHash
 end
 
 def get_japanese_emoticon
